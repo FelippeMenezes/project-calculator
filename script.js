ca4e7c8cb892c09cb4900d4d309a5cf35470bcf5
Felippe Santana <felippemenezes1982@gmail.com>
2026-04-09 18:15:32 -0300

    Comment removed

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const buttons = document.querySelectorAll("button");
 
 buttons.forEach((btn) => {
     btn.addEventListener("mousedown", () => {
-        clickDownSound.currentTime = 0; // reinicia o som (importante!)
+        clickDownSound.currentTime = 0;
         clickDownSound.play();
     });
         btn.addEventListener("mouseup", () => {
