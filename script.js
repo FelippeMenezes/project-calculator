44a4101fc4117723c28f8f85a392308104f6e452
Felippe Santana <felippemenezes1982@gmail.com>
2026-04-09 19:31:17 -0300

    Refactored click event

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const clickUpSound = new Audio("sounds/up.wav");
 const buttons = document.querySelectorAll("button");
 const inputDisplay = document.getElementById("input-display");
 
+const pi = "3.1415926536";
+
 buttons.forEach((btn) => {
     btn.addEventListener("mousedown", () => {
         clickDownSound.currentTime = 0;
@@ -14,58 +16,21 @@ buttons.forEach((btn) => {
         clickUpSound.play();
     });
     btn.addEventListener("click", () => {
-        if (btn.textContent === "1") {
-            inputDisplay.value += 1;
-        };
-    });
-    btn.addEventListener("click", () => {
-        if (btn.textContent === "2") {
-            inputDisplay.value += 2;
-        };
-    });
-    btn.addEventListener("click", () => {
-        if (btn.textContent === "3") {
-            inputDisplay.value += 3;
-        };
-    });
-        btn.addEventListener("click", () => {
-        if (btn.textContent === "4") {
-            inputDisplay.value += 4;
-        };
-    });
-        btn.addEventListener("click", () => {
-        if (btn.textContent === "5") {
-            inputDisplay.value += 5;
-        };
-    });
-        btn.addEventListener("click", () => {
-        if (btn.textContent === "6") {
-            inputDisplay.value += 6;
-        };
-    });
-        btn.addEventListener("click", () => {
-        if (btn.textContent === "7") {
-            inputDisplay.value += 7;
-        };
-    });
-        btn.addEventListener("click", () => {
-        if (btn.textContent === "8") {
-            inputDisplay.value += 8;
-        };
-    });
-        btn.addEventListener("click", () => {
-        if (btn.textContent === "9") {
-            inputDisplay.value += 9;
-        };
-    });
-        btn.addEventListener("click", () => {
-        if (btn.textContent === "0") {
-            inputDisplay.value += 0;
-        };
-    });
-        btn.addEventListener("click", () => {
-        if (btn.textContent === "π") {
-            inputDisplay.value = 3.1415926536;
-        };
+        const btnNumber = btn.textContent;
+
+        if (btnNumber === "π") {
+            inputDisplay.value = pi;
+            return;
+        }
+
+        if ("0123456789".includes(btnNumber)) {
+            if (inputDisplay.value === pi) {
+                inputDisplay.value = btnNumber;
+            } else if (inputDisplay.value === "0") {
+                inputDisplay.value = btnNumber;
+            } else {
+                inputDisplay.value += btnNumber;
+            }
+        }
     });
 });
