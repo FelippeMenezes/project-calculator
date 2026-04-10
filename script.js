a0bb5601ea19d0a7d8a3f0fbd7a5013a3355fb45
Felippe Santana <felippemenezes1982@gmail.com>
2026-04-09 21:14:33 -0300

    Clear button id and functions created

--- script.js
+++ script.js
@@ -6,6 +6,38 @@ const inputDisplay = document.getElementById("input-display");
 
 const pi = "3.1415926536";
 
+function numbersButtons(btnContent) {
+    if ("0123456789".includes(btnContent)) {
+        if (inputDisplay.value === "0" || inputDisplay.value === "3.1415926536") {
+            inputDisplay.value = btnContent;
+        } else {
+            inputDisplay.value += btnContent;
+        }
+    }
+};
+
+function piButton(btnContent) {
+    if (btnContent === "π") {
+        inputDisplay.value = pi;
+    }
+};
+
+function clearButton(btn) {
+    if (btn.id === "clear-btn") {
+        inputDisplay.value = "";
+    }
+};
+
+function updateACButton() {
+    const acButton = document.getElementById("clear-btn");
+
+    if (inputDisplay.value === "" || inputDisplay.value === "0") {
+        acButton.textContent = "AC";
+    } else {
+        acButton.textContent = "CE";
+    }
+};
+
 buttons.forEach((btn) => {
     btn.addEventListener("mousedown", () => {
         clickDownSound.currentTime = 0;
@@ -16,21 +48,11 @@ buttons.forEach((btn) => {
         clickUpSound.play();
     });
     btn.addEventListener("click", () => {
-        const btnNumber = btn.textContent;
+        const btnContent = btn.textContent;
 
-        if (btnNumber === "π") {
-            inputDisplay.value = pi;
-            return;
-        }
-
-        if ("0123456789".includes(btnNumber)) {
-            if (inputDisplay.value === pi) {
-                inputDisplay.value = btnNumber;
-            } else if (inputDisplay.value === "0") {
-                inputDisplay.value = btnNumber;
-            } else {
-                inputDisplay.value += btnNumber;
-            }
-        }
+        clearButton(btn);
+        piButton(btnContent);
+        numbersButtons(btnContent);
+        updateACButton();
     });
 });
